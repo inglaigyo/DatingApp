95883025caac0b6377e7af2c993d936510bf0836
Skand <polorace2572@gmail.com>
2022-01-24 17:38:39 +0530

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public Byte[] PasswordSalt { get; set; }
     }
 }
